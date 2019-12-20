fce47cda830dee6b872aff3794df926b8970cb13
Monkata96 <sieroteev@gmail.com>
2019-12-20 09:17:20 +0200

    Updates

---
+++ precache-manifest.5971625eb100cab093eec80cb8111c79.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2d56fc91b304c754fc901fdef08d5e",
+    "url": "/simeon12396/affiliate-2019.git/index.html"
+  },
+  {
+    "revision": "81411dffdff8e18fee08",
+    "url": "/simeon12396/affiliate-2019.git/static/css/2.ece91136.chunk.css"
+  },
+  {
+    "revision": "7d2a81de306b1c5e6846",
+    "url": "/simeon12396/affiliate-2019.git/static/css/main.5b14353b.chunk.css"
+  },
+  {
+    "revision": "81411dffdff8e18fee08",
+    "url": "/simeon12396/affiliate-2019.git/static/js/2.aa0f52f0.chunk.js"
+  },
+  {
+    "revision": "7d2a81de306b1c5e6846",
+    "url": "/simeon12396/affiliate-2019.git/static/js/main.6a4b4739.chunk.js"
+  },
+  {
+    "revision": "dc957f5ca0b8b04074c2",
+    "url": "/simeon12396/affiliate-2019.git/static/js/runtime-main.976cded6.js"
+  },
+  {
+    "revision": "1a7948b30db35062a50bb3193e4cd371",
+    "url": "/simeon12396/affiliate-2019.git/static/media/panel-bck.1a7948b3.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/simeon12396/affiliate-2019.git/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/simeon12396/affiliate-2019.git/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/simeon12396/affiliate-2019.git/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/simeon12396/affiliate-2019.git/static/media/slick.f97e3bbf.svg"
+  }
+]);
